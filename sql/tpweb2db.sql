10d1c915b8c8f5936d3657630a65f31b9623e71b
FerRobledo <106934334+FerRobledo@users.noreply.github.com>
2023-10-05 13:03:48 -0300

    Base de Datos nueva

--- sql/tpweb2db.sql
+++ sql/tpweb2db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2023 at 11:44 PM
+-- Generation Time: Oct 05, 2023 at 06:00 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,10 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `director` (
   `director_id` int(11) NOT NULL,
-  `Nombre` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
   `Apellido` varchar(45) NOT NULL,
   `Nacionalidad` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `director`
+--
+
+INSERT INTO `director` (`director_id`, `Nombre`, `Apellido`, `Nacionalidad`) VALUES
+(1, 'Kevin', 'Greutert', 'Estados Unidos'),
+(2, 'Alexandr', 'Reshetnikov', 'Rusia');
 
 -- --------------------------------------------------------
 
@@ -44,7 +52,6 @@ CREATE TABLE `peliculas` (
   `pelicula_id` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `genero` varchar(45) NOT NULL,
-  `director` varchar(45) NOT NULL,
   `fecha de lanzamiento` date NOT NULL,
   `premios` varchar(45) NOT NULL,
   `duracion en min` int(11) NOT NULL,
@@ -53,6 +60,15 @@ CREATE TABLE `peliculas` (
   `estudio de produccion` varchar(45) NOT NULL,
   `director_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `peliculas`
+--
+
+INSERT INTO `peliculas` (`pelicula_id`, `nombre`, `genero`, `fecha de lanzamiento`, `premios`, `duracion en min`, `clasificacion por edades`, `presupuesto`, `estudio de produccion`, `director_id`) VALUES
+(1, 'Saw 10', 'Thriller,Terror,Crimen,Gore', '2023-09-28', '0', 118, 'R', 13000000, 'Twisted Pictures', 1),
+(2, 'The Collection', 'Terror', '2012-09-19', '0', 82, 'R', 10000000, 'LD Entertainment', 1),
+(3, 'September 7th', 'Terror', '2023-09-05', '0', 185, 'R', 13000000, 'Emika Games', 2);
 
 --
 -- Indexes for dumped tables
@@ -70,6 +86,22 @@ ALTER TABLE `director`
 ALTER TABLE `peliculas`
   ADD PRIMARY KEY (`pelicula_id`),
   ADD KEY `director_id_idx` (`director_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `director`
+--
+ALTER TABLE `director`
+  MODIFY `director_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `pelicula_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
